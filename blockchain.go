9861ceffc9fdb406fb874bfaf3bcb5344892667d
aserafim <correia.alefe@gmail.com>
2024-10-15 10:08:24 -0300

    :sparkles: feat: Criado construtor para um novo bloco

--- blockchain.go
+++ blockchain.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 )
 
 type Block struct {
@@ -10,6 +11,16 @@ type Block struct {
 	previousHash string
 	timestamp    int64
 	transactions []string
+}
+
+func NewBlock(nonce int, previousHash string) *Block {
+	b := new(Block)
+	// Transforma o timestamp em um valor
+	// numérico
+	b.timestamp = time.Now().UnixNano()
+	b.nonce = nonce
+	b.previousHash = previousHash
+	return b
 }
 
 func init() {
