985f44febe92925d693ddc86b704afd9d7c4b964
aserafim <correia.alefe@gmail.com>
2024-10-15 10:21:25 -0300

    Criada struct da blockchain

--- blockchain.go
+++ blockchain.go
@@ -6,6 +6,23 @@ import (
 	"time"
 )
 
+type Blockchain struct {
+	transactionPool []string
+	chain           []*Block
+}
+
+func NewBlockChain() *Blockchain {
+	bc := new(Blockchain)
+	bc.CreateBlock(0, "init hash")
+	return bc
+}
+
+func (bc *Blockchain) CreateBlock(nonce int, previousHash string) *Block {
+	b := NewBlock(nonce, previousHash)
+	bc.chain = append(bc.chain, b)
+	return b
+}
+
 type Block struct {
 	nonce        int
 	previousHash string
