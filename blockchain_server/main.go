1dc14edf0d1984f11785f0b5cdfbfa1496d4d814
aserafim <correia.alefe@gmail.com>
2024-10-21 15:14:30 -0300

    :sparkles: feat: Criado servidor para wallets

--- blockchain_server/main.go
+++ blockchain_server/main.go
@@ -12,12 +12,6 @@ func init() {
 func main() {
 	port := flag.Uint("port", 5000, "TCP Port Number for Blockchain Server")
 	flag.Parse()
-	app := NewBlockChainServer(uint16(*port))
+	app := NewBlockchainServer(uint16(*port))
 	app.Run()
-
-	// imprime o endereço de memória
-	// da porta
-	// fmt.Println(port)
-	// fmt.Println(*port) imprime o número da porta
-
 }
